6228627e6f7e57b27b168f3f249b3ea1feb84a09
Nitzan Maizel <nitzanmaizel@gmail.com>
2024-10-16 17:15:01 +0300

    frontend - setup MUI RTL, tanstack and react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+import App from './App.tsx';
+import theme from './theme.tsx';
+import './index.css';
+
+const queryClient = new QueryClient();
+
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <CacheProvider value={cacheRtl}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ThemeProvider>
+        </CacheProvider>
+      </QueryClientProvider>
+    </Router>
+  </StrictMode>
+);
